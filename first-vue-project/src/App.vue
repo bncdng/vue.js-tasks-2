4314fa391ab9ba7f22efcd709bd00fd44db56380
Bianca Dang <bncdng@gmail.com>
2024-05-08 14:27:02 +0200

    add firt vue test project

---
+++ first-vue-project/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Hallo Vue</h1>
+  <MainHeader />
+</template>
+
+<script>
+// IMPORT COMPONENT
+import MainHeader from '@/components/MainHeader.vue'
+
+export default {
+  components: {
+    // REGISTER COMPONENT
+    MainHeader
+  }
+}
+</script>
